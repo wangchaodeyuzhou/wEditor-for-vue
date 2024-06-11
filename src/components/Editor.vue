ef3253d96436207dc83c31518c5faa13019010bc
王荣昌 <wangrongchang@kingsoft.com>
2024-06-11 11:35:13 +0800

    fix: 调整 ts-ignore

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -7,10 +7,10 @@ function genErrorInfo(fnName: string) {
   info += `\nPlease use '@${fnName}' event instead of props`;
   return info;
 }
-
+// @ts-ignore
 export default Vue.extend({
   //【注意】单独写 <template>...</template> 时，rollup 打包完浏览器运行时报错，所以先在这里写 template
-  render(h) {
+  render(h: Function) {
     return h('div', { ref: 'box' });
   },
   name: 'Editor',
@@ -26,40 +26,41 @@ export default Vue.extend({
   },
   watch: {
     // 监听 'value' 属性变化 - value 用于自定义 v-model
-    value(newVal) {
+    value(newVal: any) {// @ts-ignore
       const isEqual = newVal === this.curValue;
       if (isEqual) return; // 和当前内容一样，则忽略
 
       // 重置 HTML
+      // @ts-ignore
       this.setHtml(newVal);
     },
   },
   methods: {
     // 重置 HTML
-    setHtml(newHtml: string) {
+    setHtml(newHtml: string) {// @ts-ignore
       const editor = this.editor as any;
       if (editor == null) return;
       editor.setHtml(newHtml);
     },
 
     // 创建 editor
-    create() {
+    create() {// @ts-ignore
       if (this.$refs.box == null) return;
-
+     // @ts-ignore
       const defaultConfig = this.defaultConfig || {};
 
-      const defaultContent = JSON.stringify(
+      const defaultContent = JSON.stringify(// @ts-ignore
         Array.isArray(this.defaultContent) ? this.defaultContent : []
       );
 
-      createEditor({
-        selector: this.$refs.box as Element,
-        html: this.defaultHtml || this.value || '',
+      createEditor({// @ts-ignore
+        selector: this.$refs.box as Element,// @ts-ignore
+        html: this.defaultHtml || this.value || '',// @ts-ignore
         config: {
           ...defaultConfig,
-          onCreated: (editor) => {
+          onCreated: (editor) => {// @ts-ignore
             this.editor = Object.seal(editor) as any; // 注意，一定要用 Object.seal() 否则会报错
-
+            // @ts-ignore
             this.$emit('onCreated', editor);
             if (defaultConfig.onCreated) {
               const info = genErrorInfo('onCreated');
@@ -67,45 +68,46 @@ export default Vue.extend({
             }
           },
           onChange: (editor) => {
-            const editorHtml = editor.getHtml();
+            const editorHtml = editor.getHtml();// @ts-ignore
             this.curValue = editorHtml; // 记录当前 html 内容
+            // @ts-ignore
             this.$emit('input', editorHtml); // 用于自定义 v-model
-
+            // @ts-ignore
             this.$emit('onChange', editor);
             if (defaultConfig.onChange) {
               const info = genErrorInfo('onChange');
               throw new Error(info);
             }
           },
-          onDestroyed: (editor) => {
+          onDestroyed: (editor) => {// @ts-ignore
             this.$emit('onDestroyed', editor);
             if (defaultConfig.onDestroyed) {
               const info = genErrorInfo('onDestroyed');
               throw new Error(info);
             }
           },
-          onMaxLength: (editor) => {
+          onMaxLength: (editor) => {// @ts-ignore
             this.$emit('onMaxLength', editor);
             if (defaultConfig.onMaxLength) {
               const info = genErrorInfo('onMaxLength');
               throw new Error(info);
             }
           },
-          onFocus: (editor) => {
+          onFocus: (editor) => {// @ts-ignore
             this.$emit('onFocus', editor);
             if (defaultConfig.onFocus) {
               const info = genErrorInfo('onFocus');
               throw new Error(info);
             }
           },
-          onBlur: (editor) => {
+          onBlur: (editor) => {// @ts-ignore
             this.$emit('onBlur', editor);
             if (defaultConfig.onBlur) {
               const info = genErrorInfo('onBlur');
               throw new Error(info);
             }
           },
-          customAlert: (info, type) => {
+          customAlert: (info, type) => {// @ts-ignore
             this.$emit('customAlert', info, type);
             if (defaultConfig.customAlert) {
               const info = genErrorInfo('customAlert');
@@ -117,14 +119,14 @@ export default Vue.extend({
               const info = genErrorInfo('customPaste');
               throw new Error(info);
             }
-            let res;
+            let res;// @ts-ignore
             this.$emit('customPaste', editor, event, (val: any) => {
               res = val;
             });
             return res;
           },
         },
-        content: JSON.parse(defaultContent),
+        content: JSON.parse(defaultContent),// @ts-ignore
         mode: this.mode || 'default',
       });
     },
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,32 +1,33 @@
 <script lang="ts">
 import Vue from 'vue';
 import { createToolbar, DomEditor } from '@wangchaodeyuzhou/editor';
-
+// @ts-ignore
 export default Vue.extend({
   name: 'Toolbar',
-  render(h) {
+  render(h: Function) {
     return h('div', { ref: 'box' });
   },
   props: ['editor', 'defaultConfig', 'mode'],
   methods: {
     // 创建 toolbar
     create(editor: any) {
+      // @ts-ignore
       if (this.$refs.box == null) return;
       if (editor == null) return;
       if (DomEditor.getToolbar(editor)) return // 不重复创建
 
       createToolbar({
-        editor,
-        selector: this.$refs.box as Element,
-        config: this.defaultConfig || {},
+        editor,// @ts-ignore
+        selector: this.$refs.box as Element,// @ts-ignore
+        config: this.defaultConfig || {},// @ts-ignore
         mode: this.mode || 'default',
       });
     },
   },
   watch: {
     editor: {
-      handler(e) {
-        if (e == null) return;
+      handler(e: any) {
+        if (e == null) return;// @ts-ignore
         this.create(e);
       },
       immediate: true,
